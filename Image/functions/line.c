938e6d239b8fc115862b413d6fa863de1272ba13
egjimenezg <egjimenezg@gmail.com>
2015-07-27 05:26:52 -0500

    coding #4 completing DDA algorithm implementation

--- Image/functions/line.c
+++ Image/functions/line.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "image.h"
 #define ROUND_NUMBER(number) ((int)(number+0.5))
+
+int **matrix;
+
+void glBegin(){
+  matrix = createImageMatrix(H,W);
+}
 
 int getIncrement(dx,dy){
   int increment = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
@@ -8,12 +15,23 @@ int getIncrement(dx,dy){
 }
 
 void glVertex4ui(unsigned int x1,unsigned int x2,unsigned int y1,unsigned int y2){
+ 
   int dy = y2-y1;
   int dx = x2-x1;
   int increment = getIncrement(dx,dy);
-  unsigned int x = x1,y = y1;
+  float x = x1,y = y1;
+  int i = 0,j=0;
 
-  float yIncrement = dy/increment;
-  float xIncrement = dx/increment;   
+  float yIncrement = dy/(float)increment;
+  float xIncrement = dx/(float)increment;   
+
+  matrix[ROUND_NUMBER(y)][ROUND_NUMBER(x)] = 1;
+
+  for(i=0;i<increment;i++){
+    x+= xIncrement;
+    y+= yIncrement;
+    matrix[ROUND_NUMBER(y)][ROUND_NUMBER(x)] = 1;
+  }
+  
+  createImage(W,H,matrix);
 }
-
