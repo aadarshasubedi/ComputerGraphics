ad492b6f1f7c6bb658b18996e62b9c9c41ea21df
egjimenezg <egjimenezg@gmail.com>
2015-07-27 05:25:34 -0500

    coding #4 adding function for image matrix creation

--- Image/functions/image.c
+++ Image/functions/image.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-void createImage(int x,int y){
+int** createImageMatrix(int height,int width){
+  int i,j;
+  int** matrix = (int**)malloc(height*sizeof(int*));
+
+  for(i=0;i<height;i++)
+    matrix[i] = (int*)malloc(width*sizeof(int));
+  
+  for(i=0;i<height;i++)
+    for(j=0;j<width;j++) 
+      matrix[i][j] = 0;
+
+  return matrix; 
+}
+
+void createImage(int x,int y,int** imageMatrix){
   int i,j;
   static unsigned char color[3];  
   
@@ -17,12 +31,20 @@ void createImage(int x,int y){
   fprintf(f,"P6\n%d %d\n255\n",x,y);
   
   srand(time(NULL));
-  
+
   for(i=0;i<y;i++){
     for(j=0;j<x;j++){
-      color[0] = rand() % 256;
-      color[1] = rand() % 256;
-      color[2] = rand() % 256;
+      if(imageMatrix[i][j] == 1){
+        color[0] = 255;
+        color[1] = 255; 
+        color[2] = 255; 
+      }
+      else{
+        color[0] = 0; 
+        color[1] = 0;
+        color[2] = 0;
+      }
+      
       fwrite(color,1,3,f);
     }
   }
